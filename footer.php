c6f9217cc08bda89058259c8f8ab865f5cc682e8
shii-shimi <you@example.com>
2022-03-05 18:24:00 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,19 @@
+</div>
+<footer>
+        <div class="foot">
+            <img class="fimg" src="<?php bloginfo('template_url'); ?>/img/foot.jpg" alt="フッター画像">
+            <p class="company">@shiicreer . desing</p>
+
+        </div>
+</footer>
+<?php wp_footer(); ?>
+
+<!--js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+
+<script src="script.js"></script>
+</body>
+</html>
