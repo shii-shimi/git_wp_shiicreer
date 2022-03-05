589f9afa1ee9402821a59625036ba1bc371935e6
shii-shimi <you@example.com>
2022-03-05 18:25:12 +0900

    first commit

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+	<link rel="stylesheet" href="style.css">
+
+	<link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+	<link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/7-1-20/css/7-1-20.css">
+
+<!------------アイコン-------------------------------->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
+
+<!--bootsyrap-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<!--js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+	<?php wp_head(); ?>
+
+	
+</head>
+<body>
+	<header>
+
+	 <!---------ハンバーガーメニュー---------->
+	 <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          
+          <nav class="globalMenuSp">
+              <ul>
+			 	 <li><a class="menuclose" href="<?php bloginfo('url'); ?>#t">top</a></li>
+                  <li><a class="menuclose" href="<?php bloginfo('url'); ?>#1">about</a></li>
+                  <li><a class="menuclose" href="<?php bloginfo('url'); ?>#2">work</a></li>
+                  <li><a class="menuclose" href="<?php bloginfo('url'); ?>#3">contact</a></li>
+              </ul>
+          </nav>
+
+	</header>
+	<div class="main">
