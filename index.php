3ae34dac9e7d7b66479b1ce8d99915539bce9e70
shii-shimi <you@example.com>
2022-03-07 14:42:44 +0900

    hennkou

--- index.php
+++ index.php
@@ -6,25 +6,39 @@
 
 		<!-- 記事のリンク -->
 		<div class="news__items">
-			<a href="<?php the_permalink(); ?>">
-
-					<!-- 投稿日 -->
-					<time datetime="<?php echo get_the_date('Y-m-d'); ?>">
-						<?php echo get_the_date(); ?>
-					</time>
-
+			
+			
 				<!-- アイキャッチ画像 -->
 				<?php the_post_thumbnail('full'); ?>
-				<!-- タイトル -->
-				
-					<p>
-						<?php the_title(); ?>
-					</p>
-				
 
+
+				<div class="news__lite">
+					<!-- 投稿日 -->
+					<div class="news__time">
+						<time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+							<?php echo get_the_date(); ?>
+						</time>
+					</div>
+					<a href="<?php the_permalink(); ?>">
+					<!-- タイトル -->
+						<div class="news__tittle">
+							<p>
+								<?php the_title(); ?>
+							</p>
+						</div>
+					</a>
+				</div>
 				
-			</a>
 		</div>
 <?php endwhile;
 endif; ?>
+
+	<div class="btnbox">
+            <a href="http://localhost/wp_shiicreer/wordpress/" class="btn btn--orange btn--circle btn--circle-a btn--shadow">
+                home
+                <img src="<?php bloginfo('template_url'); ?>/img/streamline-icon-interface-edit-pin-3@48x48.png" alt="" class="pin">
+            </a>
+    </div>
+
+
 <?php get_footer(); ?>
